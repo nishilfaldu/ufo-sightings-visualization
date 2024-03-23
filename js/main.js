df68090527ad91caee7fe14db45bc667c41af16b
Zenith Zack <faldund@outlook.com>
2024-03-23 14:42:43 -0400

    feat(client): add a main script to load data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-// fetch data here
+d3.csv('data/ufoSample.csv')
+.then(data => {
+    console.log(data[0]);
+    console.log(data.length);
+    data.forEach(d => {
+      // console.log(d);
+      d.latitude = +d.latitude; //make sure these are not strings
+      d.longitude = +d.longitude; //make sure these are not strings
+    });
+
+    // Initialize chart and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+
+  })
+  .catch(error => console.error(error));
