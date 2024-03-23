99607a52146efa51f35573189f71383214caaff5
Zenith Zack <faldund@outlook.com>
2024-03-23 15:11:51 -0400

    feat(client): add a filter for NaN / null values of latitude and longitude

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 d3.csv('data/ufo_sightings.csv')
 .then(data => {
-    console.log(data[0]);
+    console.log(data);
     console.log(data.length);
+
+    let notMappedCount = 0;
     data.forEach(d => {
-      // console.log(d);
-      d.latitude = +d.latitude; //make sure these are not strings
-      d.longitude = +d.longitude; //make sure these are not strings
-    });
+        d.latitude = d.latitude && !isNaN(d.latitude) ? +d.latitude : null;
+        d.longitude = d.longitude && !isNaN(d.longitude) ? +d.longitude : null;
+        d.encounter_length = d.encounter_length ? +d.encounter_length : null;
+        if (!d.latitude || !d.longitude) {
+            console.log(d, "d")
+            notMappedCount++;
+        }
+      });
+
+    // Filter out data points with null latitude or longitude
+    data = data.filter(d => d.latitude !== null && d.longitude !== null);
+
+    // Display the count of sightings not mapped
+    const notMappedCountElement = document.getElementById("sightings-not-mapped");
+    notMappedCountElement.innerHTML = `Sightings not mapped: ${notMappedCount}`;
+    // document.getElementById('my-map').appendChild(notMappedCountElement);
 
     // Initialize chart and then show it
     leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
