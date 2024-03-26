6ff329e5fa456ce55468c7ef8f1364df6ff78ab0
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2024-03-26 13:56:04 -0400

    removed one of the backgrounds

--- js/main.js
+++ js/main.js
@@ -165,15 +165,15 @@ const attributes = {
             attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             ext: 'png'
         })
-    },
-    "Stadia_StamenToner": {
-        label: "Stadia (Stamen Toner)",
-        layer: L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
-            minZoom: 0,
-            maxZoom: 20,
-            attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            ext: 'png'
-        })
+    // },
+    // "Stadia_StamenToner": {
+    //     label: "Stadia (Stamen Toner)",
+    //     layer: L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
+    //         minZoom: 0,
+    //         maxZoom: 20,
+    //         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //         ext: 'png'
+    //     })
     }
     // Add more options for different map backgrounds
 };
